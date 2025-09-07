ac2b77e139a2d53ecf920dc7467fd75ca5a3407e
RailsonF <railsonfurtado16@gmail.com>
2025-09-07 16:33:00 -0300

    ajuste no titulo da pagina

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/avaliacao-atendimento/"
+  base: '/avaliacao-atendimento/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  },
+  // Adicione esta seção para configurar variáveis HTML
+  define: {
+    'import.meta.env.VITE_APP_TITLE': JSON.stringify('Seu Título Personalizado')
+  }
 })
