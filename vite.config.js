1ef5e02dca3ff56ea0f44069a50ad8acff858aa8
RailsonF <railsonfurtado16@gmail.com>
2025-09-07 17:29:13 -0300

    Ajuste de permissão para o render

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,13 @@ export default defineConfig({
       }
     }
   },
+  preview: {
+    allowedHosts: [
+      'avaliacao-atendimento-5xjl.onrender.com', // Domínio do Render
+      'localhost',                               // Desenvolvimento local
+      '127.0.0.1'                                // Backup para localhost
+    ]
+  },
   // Adicione esta seção para configurar variáveis HTML
   define: {
     'import.meta.env.VITE_APP_TITLE': JSON.stringify('Seu Título Personalizado')
